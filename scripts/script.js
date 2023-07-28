cf06f31f26e632957d26d777cf50f1fc04c2f823
nnda <96333146+nndda@users.noreply.github.com>
2023-07-29 06:17:03 +0800

    Add fallback for CDNs

--- scripts/script.js
+++ scripts/script.js
@@ -9,24 +9,65 @@ function scrollToSection(target){
   document.getElementById( target ).scrollIntoView({ behavior: 'smooth' });
 };
 
+
 // Masonry gallery layout
 
-var $gallery = $('.gallery').masonry({
-  itemSelector: '.gallery-item',
-  columnWidth: '.gallery-item',
-  percentPosition: true,
-  fitWidth: true,
-  transitionDuration: 0,
-  gutter: 4
-});
+var masonry_init = false;
 
-$gallery.imagesLoaded().progress( function() {
-  $gallery.masonry('layout');
-});
+function initMasonry(){
+  if (
+    $('.gallery').masonry &&
+    $('.gallery').imagesLoaded() &&
+    !masonry_init){
+    console.log('masonry loaded');
 
-// Viewer
+    var $gallery = $('.gallery').masonry({
+      itemSelector: '.gallery-item',
+      columnWidth: '.gallery-item',
+      percentPosition: true,
+      fitWidth: true,
+      transitionDuration: 0,
+      gutter: 4
+    });
 
-new VenoBox({
-  selector: '.view-item',
-  spinner: 'wander'
-});
+    $gallery.imagesLoaded().progress( function() {
+      $gallery.masonry('layout');
+    });
+
+    masonry_init = true;
+  } else { console.log('masonry init failed'); }
+};
+
+// initMasonry();
+
+
+// Lightbox
+
+var lightbox_init = false;
+
+function initLightbox(){
+  if (
+    typeof VenoBox === 'function'
+    && !lightbox_init){
+
+    console.log('lightbox loaded');
+
+    new VenoBox({
+      selector: '.view-item',
+      spinner: 'wander'
+    });
+
+    lightbox_init = true;
+  } else { console.log('lightbox init failed : ', typeof VenoBox); }
+};
+
+// initLightbox();
+
+
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    console.log('document loaded');
+    initMasonry();
+    initLightbox();
+  }
+};
